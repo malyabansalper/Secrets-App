c3d53e04b5666f897eaa9c471c026f9fce082dda
Malya Bansal <68691583+malyabansalper@users.noreply.github.com>
2022-02-02 11:52:24 +0530

    Update app.js

--- app.js
+++ app.js
@@ -180,13 +180,15 @@ app.post("/login", function(req, res){
 
 
 
-let port = process.env.PORT;
-if (port == null || port == "") {
-  port = 3000;
-}
+// let port = process.env.PORT;
+// if (port == null || port == "") {
+//   port = 3000;
+// }
 
 
 
-app.listen(port, function() {
-  console.log("Server has started Successfully.");
-});
+// app.listen(port, function() {
+//   console.log("Server has started Successfully.");
+// });
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
